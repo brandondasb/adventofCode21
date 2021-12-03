665eaea219dbd623bc947ec0b95b7e80ea3dfb55
brandon.milambo <brandon.milambo@autotrader.co.uk>
2021-12-03 13:13:00 +0000

    Day1 part one

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,43 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(depthChecker(input))
+//    println(part1(input))
+//    println(part2(input))
 }
+
+fun depthChecker(input: List<String>): Int {
+    val firstMeasurement = input[0]
+    var increaseCounter = 0
+    input.subList(1, input.size).forEachIndexed { index, depth ->
+        val previous = if (index == 0) {
+            firstMeasurement
+        } else {
+            input[index]
+        }
+
+        if (hasIncreased(previous, depth)) {
+            increaseCounter++
+        } else {
+            println("hold up:$previous is higher than $depth.")
+        }
+    }
+    return increaseCounter
+}
+
+
+fun hasIncreased(previous: String, currentMeasurement: String): Boolean {
+    return (previous.toInt() < currentMeasurement.toInt())
+}
+/**
+ * signal strenght 0-50 stars
+ * sonar sweep report(puzzle input)
+ *  how quickly the depth increases
+ *  count the number of time the depth increase from previous measurement
+ *  no measurement before the first measurement
+ *
+ */
